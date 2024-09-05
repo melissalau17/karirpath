2963532b5b7bd68c332b3cd2a19dd81482b2485f
Aldi <alfahluzi@gmail.com>
2024-09-05 07:41:23 +0700

    Restructuring files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Header from './Header';
-import SignInForm from './SignInForm';
-import RegisterForm from './RegisterForm';
-import HomePage from './HomePage';
-import JobResultsPage from './JobResultsPage';
-import ProfilePage from './ProfilePage';
-import JobsPage from './JobsPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/page/HomePage";
+import SignInPage from "./components/page/SignInPage";
+import RegisterPage from "./components/page/RegisterPage";
+import JobResultsPage from "./components/page/JobResultsPage";
+import JobsPage from "./components/page/JobsPage";
+import Header from "./components/ui/Header";
+import ProfilePage from "./components/page/ProfilePage";
 
 function App() {
-    return (
-        <Router>
-            <div style={styles.container}>
-                <Header />
-                <div style={styles.mainContent}>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/signin" element={<SignInForm />} />
-                        <Route path="/signup" element={<RegisterForm />} />
-                        <Route path="/jobs/:jobTitle" element={<JobResultsPage />} />
-                        <Route path="/profile" element={<ProfilePage />} />
-                        <Route path="/my-jobs" element={<JobsPage />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    )
+	return (
+		<Router>
+			<div style={styles.container}>
+				<Header />
+				<div style={styles.mainContent}>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/signin" element={<SignInPage />} />
+						<Route path="/signup" element={<RegisterPage />} />
+						<Route path="/jobs/:jobTitle" element={<JobResultsPage />} />
+						<Route path="/profile" element={<ProfilePage />} />
+						<Route path="/my-jobs" element={<JobsPage />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    header: {
-        width: '100%',
-        backgroundColor: '#01212E', 
-        color: '#fff',
-        textAlign: 'center',
-    },
-    mainContent: {
-        display: 'flex',
-    },
+	container: {
+		display: "flex",
+		flexDirection: "column",
+	},
+	header: {
+		width: "100%",
+		backgroundColor: "#01212E",
+		color: "#fff",
+		textAlign: "center",
+	},
+	mainContent: {
+		display: "flex",
+	},
 };
 
 export default App;
